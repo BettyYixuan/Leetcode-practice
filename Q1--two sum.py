e33b7ad24890d93db4031e8ec71ce90eed49b33f
BettyYixuan <u3629195@connect.hku.hk>
2025-03-05 18:15:47 +0800

    1. fixed some error in the codes. 2. added an explanation line

--- Q1--two sum.py
+++ Q1--two sum.py
@@ -1,11 +1,12 @@
 def main():
-    nums = list(input("请输入一个数组：").split(","))
+    nums = list(map(int,input("请输入一个数组：").split(",")))
     target = int(input("请输入目标数字："))
     length = len(nums)
     for i in range(length):
-        for j in range(length):
-            if ((nums[i]+nums[j])==target) and i != j:
-                return [i, j]
-
-print(main())
-#Q1--two sum
+        for j in range(i+1, length):
+            if nums[i]+nums[j]==target:
+                return [i, j], target, nums
+    return None
+result, target, nums = main()
+print(result)
+print(f'Because nums[{result[0]}] + nums[{result[1]}] == {target}, we return {result}.')
